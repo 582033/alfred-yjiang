7412b77a968fe1fbd769909e0915ac80918f8339
Yan Jiang <j582033@163.com>
2021-05-12 11:32:27 +0800

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module alfred-yjiang
 
 go 1.16
+
+require (
+	github.com/deanishe/awgo v0.28.0 // indirect
+	github.com/magefile/mage v1.11.0 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -20,16 +20,15 @@ func inputApater(inputString string) string {
 	output := "未识别的参数"
 
 	regexSlice := []regexRow{
-		{regex: `\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}`, action: libs.ActionIp(inputString)},
-		{regex: `\d+`, action: libs.ActionInttotime(inputString)},
-		{regex: `\d{4}-\d{1,2}-\d{1,2}`, action: libs.ActionTimetoint(inputString)},
+		{regex: `^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$`, action: libs.ActionIp(inputString)},
+		{regex: `^\d+$`, action: libs.ActionInttotime(inputString)},
+		{regex: `^\d{4}-\d{1,2}-\d{1,2}$`, action: libs.ActionTimetoint(inputString)},
 		{regex: `now`, action: libs.ActionGetNow(inputString)},
 	}
 
 	for _, regexObj := range regexSlice {
 		//fmt.Println(ipObj.regex)
 		match, _ := regexp.MatchString(regexObj.regex, inputString)
-		//fmt.Println(match)
 		if match {
 			output = regexObj.action
 		}
@@ -38,12 +37,13 @@ func inputApater(inputString string) string {
 }
 
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Println("请输入参数")
-		os.Exit(1)
+	args := os.Args
+	output := ""
+	if len(args) <= 0 {
+		output = "请输入参数"
+	} else {
+		input := args[1]
+		output = inputApater(input)
 	}
-
-	input := os.Args[1]
-	output := inputApater(input)
 	fmt.Println(output)
 }
